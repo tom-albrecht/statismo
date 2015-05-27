f2da6e61894910edea341b199ede64431286ed2f
Christoph Langguth <christoph.langguth@unibas.ch>
2015-05-28 00:35:18 +0200

    WIP (still BROKEN)

--- modules/ITK/examples/itkActiveShapeModelTest.cxx
+++ modules/ITK/examples/itkActiveShapeModelTest.cxx
@@ -15,9 +15,11 @@
 #include <itkImage.h>
 #include <itkImageFileReader.h>
 #include <itkStandardMeshRepresenter.h>
+#include <ASMFitter.h>
 #include "ASMFeatureExtractor.h"
 #include "itkASMNormalDirectionGradientGaussianFeatureExtractor.h"
 #include "itkActiveShapeModel.h"
+#include "itkASMNormalDirectionPointSampler.h"
 
 typedef itk::Mesh<float, 3> MeshType;
 typedef itk::Image<float, 3> ImageType;
@@ -35,7 +37,8 @@ int main(int argc, char *argv[]) {
     ActiveShapeModelType::Pointer aModel = ActiveShapeModelType::New();
     new ActiveShapeModelType;
 
-    std::string modelname("/home/langguth/workspaces.exported/stk.idea/bladderdemo/src/main/resources/bladder/asmModels/asmLevel-1.h5");
+    //std::string modelname("/home/langguth/workspaces.exported/stk.idea/bladderdemo/src/main/resources/bladder/asmModels/asmLevel-1.h5");
+    std::string modelname("/tmp/asmLevel-1.h5");
     RepresenterType::Pointer representer = RepresenterType::New();
     aModel->Load(representer, modelname.c_str());
 
@@ -44,29 +47,27 @@ int main(int argc, char *argv[]) {
     std::cout << "statismo::SSM impl object: " << aModel->GetStatisticalModel()->GetstatismoImplObj() << std::endl;
     StatisticalModelType::Pointer s = aModel->GetStatisticalModel();
 
-    ImageReaderType::Pointer reader = ImageReaderType::New();
-    reader->SetFileName("/home/langguth/workspaces.exported/stk.idea/bladderdemo/src/main/resources/bladder/volumes/24.vtk");
-    reader->Update();
-    ImageType::Pointer image = reader->GetOutput();
+    itk::ASMNormalDirectionPointSampler::Pointer fitSampler = itk::ASMNormalDirectionPointSampler::New();
+    fitSampler->SetNumberOfPoints(42);
+    fitSampler->SetPointSpacing((float)0.5);
 
-//
-//
-    MeshType::Pointer mesh = aModel->GetStatisticalModel()->DrawMean();
-//
-//    itk::ASMNormalDirectionPointSampler<ASM>::Pointer sampler = itk::ASMNormalDirectionPointSampler<ASM>::New();
-//    //sampler->Init(90, (float)(2.0/3.0));
-//    sampler->Init(25, (float)(0.5));
-//    //sampler->Init(125, (float)(0.5));
-//
-//    itk::ASMFitterConfiguration::Pointer fitConfig = itk::ASMFitterConfiguration::New();
-//    fitConfig->Set1()
-//    fitConfig->Set1()
-//    fitConfig->Set1()
+    statismo::ASMFitterConfiguration fitConfig(1,2,3);
 
-//    itk::Point
+//    ImageReaderType::Pointer reader = ImageReaderType::New();
+//    reader->SetFileName("/tmp/24.vtk");
+//    reader->Update();
+//    ImageType::Pointer image = reader->GetOutput();
+//    MeshType::Pointer mesh = aModel->GetStatisticalModel()->DrawMean();
+
+    typename itk::ASMFitter::Pointer fitter = itk::ASMFitter::New();
+    fitter->SetSourceModel(aModel); // "source"
+    fitter->SetTargetImage(image); // "target"
+    fitter->SetSamplingStrategy(fitSampler); // "how"
+    fitter->SetThresholds(fitConfig); // "success criteria"
+    fitter->SetMaxIterations(42); // "how often"
+
 //
 //    typename itk::ASMFitter<ASM>::Pointer fitter = itk::ASMFitter<ASM>::New();
-//    typename itk::ASMNormalDirectionPointSampler<ASM>::Pointer pointSampler = itk::ASMNormalDirectionPointSampler<ASM>::New();
 //    fitter->Init(fitConfig, aModel, mesh, image, pointSampler);
 //    for (int i=0; i < 25; ++i) {
 //        std::cout << "iteration " << i << " start" << std::endl;
@@ -78,7 +79,7 @@ int main(int argc, char *argv[]) {
 //        std::cout << "coeffs (adj)" << result->GetCoefficients() << std::endl;
 //        fitter = fitter->SetPointset(result->GetMesh());
 //    }
-    std::cout << "Program ends" << std::endl;
+    std::cout << std::endl << "#### Program ends here ####" << std::endl << std::endl;
     return 0;
 }
 //
--- modules/ITK/include/itkASMNormalDirectionPointSampler.h
+++ modules/ITK/include/itkASMNormalDirectionPointSampler.h
@@ -1,42 +1,80 @@
-///*
-// * This file is part of the statismo library.
-// *
-// * Author: Christoph Langguth (christoph.langguth@unibas.ch)
-// *
-// * Copyright (c) 2011-2015 University of Basel
-// * All rights reserved.
-// *
-// * Redistribution and use in source and binary forms, with or without
-// * modification, are permitted provided that the following conditions
-// * are met:
-// *
-// * Redistributions of source code must retain the above copyright notice,
-// * this list of conditions and the following disclaimer.
-// *
-// * Redistributions in binary form must reproduce the above copyright
-// * notice, this list of conditions and the following disclaimer in the
-// * documentation and/or other materials provided with the distribution.
-// *
-// * Neither the name of the project's author nor the names of its
-// * contributors may be used to endorse or promote products derived from
-// * this software without specific prior written permission.
-// *
-// * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
-// * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
-// * LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS
-// * FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
-// * HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
-// * SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED
-// * TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR
-// * PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF
-// * LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
-// * NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
-// * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
-// *
-// */
-//
-//#ifndef STATISMO_ASMNORMALDIRECTIONPOINTSAMPLER_H
-//#define STATISMO_ASMNORMALDIRECTIONPOINTSAMPLER_H
+/*
+ * This file is part of the statismo library.
+ *
+ * Author: Christoph Langguth (christoph.langguth@unibas.ch)
+ *
+ * Copyright (c) 2011-2015 University of Basel
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ *
+ * Redistributions of source code must retain the above copyright notice,
+ * this list of conditions and the following disclaimer.
+ *
+ * Redistributions in binary form must reproduce the above copyright
+ * notice, this list of conditions and the following disclaimer in the
+ * documentation and/or other materials provided with the distribution.
+ *
+ * Neither the name of the project's author nor the names of its
+ * contributors may be used to endorse or promote products derived from
+ * this software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+ * LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS
+ * FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
+ * HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+ * SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED
+ * TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR
+ * PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF
+ * LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
+ * NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+ * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ *
+ */
+
+#ifndef STATISMO_ASMNORMALDIRECTIONPOINTSAMPLER_H
+#define STATISMO_ASMNORMALDIRECTIONPOINTSAMPLER_H
+
+#include <itkObject.h>
+#include <itkMacro.h>
+
+namespace itk {
+    class ASMNormalDirectionPointSampler: public Object {
+    public:
+        /* standard ITK typedefs and macros */
+        typedef ASMNormalDirectionPointSampler Self;
+        typedef Object Superclass;
+        typedef SmartPointer <Self> Pointer;
+        typedef SmartPointer<const Self> ConstPointer;
+        itkNewMacro( Self );
+        itkTypeMacro( Self, Object);
+
+
+        unsigned int GetNumberOfPoints() const {
+            return m_numberOfPoints;
+        }
+
+        void SetNumberOfPoints(unsigned int numberOfPoints) {
+            m_numberOfPoints = numberOfPoints;
+        }
+
+        float GetPointSpacing() const {
+            return m_pointSpacing;
+        }
+
+        void SetPointSpacing(float pointSpacing) {
+            m_pointSpacing = pointSpacing;
+        }
+
+    private:
+        unsigned int m_numberOfPoints;
+        float m_pointSpacing;
+    };
+
+}
 //
 //#include "itkPointsLocator.h"
 //#include "itkASMPointSampler.h"
@@ -166,4 +204,4 @@
 //
 //
 //}
-//#endif //STATISMO_ASMNORMALDIRECTIONPOINTSAMPLER_H
+#endif //STATISMO_ASMNORMALDIRECTIONPOINTSAMPLER_H
